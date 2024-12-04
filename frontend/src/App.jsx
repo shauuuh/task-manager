a8d5dbadb1ddf96c28cb0cd10d4d3fd52878e3f4
shauuuh <shauryhsw@gmail.com>
2024-12-04 00:11:53 -0600

    app updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,6 @@ import TaskManager from './components/TaskManager'
 import './App.css'
 
 function App() {
-<<<<<<< HEAD
-  const [count, setCount] = useState(0)
-=======
->>>>>>> c6a6eed (added schemas, models)
 
   return (
     <>
