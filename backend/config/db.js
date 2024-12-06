ac0eac9c15a2fb8e635f5f128bc9edffd58049bf
shauuuh <shauryhsw@gmail.com>
2024-12-06 13:50:51 -0600

    db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 /* --- Conexión bd --- */
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'postgres',
+    port: process.env.DB_PORT,
 });
 
 export default sequelize;
