ba56ec8a096b5b2817347b8a65c39ffa4c02cbb7
shauuuh <shauryhsw@gmail.com>
2024-12-07 13:40:01 -0600

    url cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors({
-  origin: 'https://task-manager-j6934amx3-shauuuhs-projects.vercel.app',
+  origin: 'https://task-manager-git-main-shauuuhs-projects.vercel.app/',
   optionsSuccessStatus: 200
 }));
 
